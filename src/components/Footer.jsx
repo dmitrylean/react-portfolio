e0e0f6c4005831933564fa7984b2aeaba7253535
DM!TRY <adam.lean.92@gmail.com>
2025-09-18 00:31:46 +0800

    add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,8 @@ import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
+  faBehance,
+  faTelegram
 } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
@@ -12,6 +14,12 @@ const Footer = () => {
         <div className="socials">
           <a href="https://github.com/dmitrylean" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://www.behance.net/dmitrylean" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faBehance} />
+          </a>
+          <a href="https://t.me/bionicman451" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTelegram} />
           </a>
         </div>
         <p>Copyright ©2020 All rights reserved</p>
